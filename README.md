d671c02737a4e1b8f2867d76f8a13c435789568a
Glen Langer <contao.modul@glen-langer.de>
2012-09-02 15:48:25 +0200

    Zwischenstand, Cronjob Start noch nicht funktional

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-cron_info
-=========
+Cron-Informations
+=================
 
-Contao - Cron Informationen
+## About
+
+Cron-Overview:
+* last execution time
+* registrations
+
+
+## Branches
+
+**master** This is the current stable branch.
+
+**dev** This is the current unstable upstream development branch.
+
+**uat** This is the current release candidate branch. (see tags with "rc" in name)
+
+
+This software using the Fugue Icons from http://p.yusukekamiyamane.com/icons/search/fugue/
---
+++ languages/en/tl_cron_info.php
@@ -0,0 +1,28 @@
+<?php 
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package   CronInfo 
+ * @author    Glen Langer 
+ * @license   LGPL 
+ * @copyright Glen Langer 2012 
+ */
+
+$GLOBALS['TL_LANG']['CronInfo']['title_time'] = 'Cron execution time';
+$GLOBALS['TL_LANG']['CronInfo']['title_reg']  = 'Cron registration';
+
+$GLOBALS['TL_LANG']['CronInfo']['cron_time_interval'] = 'Cron time interval';
+$GLOBALS['TL_LANG']['CronInfo']['cron_last_start']    = 'Last start';
+$GLOBALS['TL_LANG']['CronInfo']['interval_monthly']   = 'Monthly';
+$GLOBALS['TL_LANG']['CronInfo']['interval_weekly']    = 'Weekly';
+$GLOBALS['TL_LANG']['CronInfo']['interval_daily']     = 'Daily';
+$GLOBALS['TL_LANG']['CronInfo']['interval_hourly']    = 'Hourly';
+$GLOBALS['TL_LANG']['CronInfo']['interval_minutely']  = 'Minutely';
+$GLOBALS['TL_LANG']['CronInfo']['calendar_week']      = 'calendar week';
+$GLOBALS['TL_LANG']['CronInfo']['class']         = 'Class';
+$GLOBALS['TL_LANG']['CronInfo']['method']        = 'Method';
+$GLOBALS['TL_LANG']['CronInfo']['start']         = array('Start','Now start the cronjob');
+$GLOBALS['TL_LANG']['CronInfo']['start_confirm'] = 'Now start this cronjob?';
