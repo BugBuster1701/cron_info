69584ed5d45c0d6d0f8b647c07a9f8a4ab09c453
Glen Langer <contao.modul@glen-langer.de>
2015-08-20 22:40:45 +0200

    add laguage files (ru)

---
+++ languages/ru/modules.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://docs.transifex.com/faq/#translating
+ * @link https://www.transifex.com/projects/p/contao-cron_info/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-08-20T22:39:56+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['cron_info']['0'] = 'Информация cron';
+$GLOBALS['TL_LANG']['MOD']['cron_info']['1'] = 'Информация cron';
+
